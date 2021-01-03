cd4a0e054bb6432d5d39a9fa4a22c8f68d1ebfa7
杨 <792269881@qq.com>
2021-01-03 11:54:56 +0800

    增加了effect功能

--- pages/warn/index.js
+++ pages/warn/index.js
@@ -1,5 +1,9 @@
 import styles from './index.module.css'
+import {useEffect} from 'react'
 export default function warn(){
+    useEffect(()=>{
+        console.log('1');
+    });
     return ( <div className={styles.ww}>
         456
         </div>
